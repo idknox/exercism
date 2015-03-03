040529b3e447c7355ab4b00a1336ada8497d775d
Ian Knox <knoxid@gmail.com>
2015-03-03 13:29:04 -0700

    ana refactor

--- python/anagram/anagram.py
+++ python/anagram/anagram.py
@@ -1,5 +1,2 @@
-def alphabetize(word):
-    return ''.join(sorted(word.lower()))
-
 def detect_anagrams(test_word, possible_anagrams):
-    return [word for word in possible_anagrams if alphabetize(test_word) == alphabetize(word) and test_word.lower() != word.lower()]
+    return [word for word in possible_anagrams if sorted(test_word.lower()) == sorted(word.lower()) and test_word.lower() != word.lower()]
