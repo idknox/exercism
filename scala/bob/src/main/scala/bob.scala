b4625ed8d44f1975e1c0b9eeef1b0434dcecc633
Ian Knox <knoxid@gmail.com>
2015-07-13 21:33:59 -0600

    refactored scala bob

--- scala/bob/src/main/scala/bob.scala
+++ scala/bob/src/main/scala/bob.scala
@@ -2,19 +2,10 @@ package bob
 
 class Bob() {
   def hey(speech: String): String = {
-    val stripped_speech: String = speech.replaceAll("[, ]", "")
-    val last_char = speech takeRight 1
 
-    if (speech == "" || speech.matches("\\W+")) {
-      "Fine. Be that way!"
-    } else if (stripped_speech.forall(_.isDigit)) {
-      "Whatever."
-    } else if (speech == speech.toUpperCase() && speech.exists(_.isLetter)) {
-      "Woah, chill out!"
-    } else if (last_char == "?") {
-      "Sure."
-    } else {
-      "Whatever."
-    }
+    if (speech.trim.isEmpty) "Fine. Be that way!"
+    else if (speech == speech.toUpperCase() && speech.exists(_.isLetter)) "Woah, chill out!"
+    else if (speech.endsWith("?")) "Sure."
+    else "Whatever."
   }
 }
